7920ef9d0491c0ba9a43c570575119548e7affad
Matheus Mello <matheusmello_22@hotmail.com>
2020-10-25 17:41:29 -0300

    add files

---
+++ build/precache-manifest.3c664f953a1918400ce4d32f9b2fa0d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61dcecac787985e475e7d58055fda255",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22b51d77e28645976b4",
+    "url": "/static/css/main.416c8298.chunk.css"
+  },
+  {
+    "revision": "6cc54e3f21834bf9f0cd",
+    "url": "/static/js/2.ab024667.chunk.js"
+  },
+  {
+    "revision": "c9c39d4096006a4e7defda1932809f98",
+    "url": "/static/js/2.ab024667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22b51d77e28645976b4",
+    "url": "/static/js/main.a7d384d6.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "10f786d262d2ece58f1f8a5172e06fc3",
+    "url": "/static/media/logo.10f786d2.jpeg"
+  },
+  {
+    "revision": "4893a5546c0277993681f06c15f51c80",
+    "url": "/static/media/star_in.4893a554.svg"
+  }
+]);
